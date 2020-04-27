46bcf67b064e1883ea1412c99ee67aaf2e4d2d51
jayesh1147 <naidujayesh04@gmail.com>
2020-04-27 18:52:07 +0530

    splash scrren and logo added

---
+++ app/src/main/java/com/example/enrichyourknowlwdge/splash.java
@@ -0,0 +1,29 @@
+package com.example.enrichyourknowlwdge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+
+            public void run() {
+
+                startActivity(new Intent(splash.this,StartingScreenActivity.class));
+                finish();
+
+
+            }
+        },3000);
+    }
+}
